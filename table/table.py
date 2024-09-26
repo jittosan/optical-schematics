b009032b5d314340dc4f9a453a442dac5c11996f
Jitto <64718148+jittosan@users.noreply.github.com>
2024-09-26 16:58:55 +0800

    include an Optical Table to place objects onto

--- table/table.py
+++ table/table.py
@@ -1,27 +1,50 @@
-
-import matplotlib as plt    
+from pyx import canvas, path, style, color, deco
+from pyx import bitmap
+import os
 
 class OpticalTable:
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
         self.components = []
+        self.grid = False
+        self.points = False
+        self.canvas_padding = 3
     
-    def add_component(self, component):
+    def add(self, component):
         self.components.append(component)
     
     def draw(self):
-        fig, ax = plt.subplots()
-        # Draw table as a grid of very small dots
-        ax.set_aspect('equal')
-        for x in range(self.cols):
-            for y in range(self.rows):
-                ax.plot(x, y, 'ko', markersize=2)  # Small black dots as mounting holes
+        # Create a canvas
+        c = canvas.canvas()
+        # put a white background
+        c.fill(path.rect(-self.canvas_padding, -self.canvas_padding, self.cols+2*self.canvas_padding, self.rows+2*self.canvas_padding), [color.rgb.white])
+        # if grid is enabled
+        if self.grid:
+            for i in range(self.rows):
+                c.stroke(path.line(0, i, self.cols, i), [style.linewidth.Thin, color.grey(0.8)])
+            for i in range(self.cols):
+                c.stroke(path.line(i, 0, i, self.rows), [style.linewidth.Thin, color.grey(0.8)])
+        # if points are enabled
+        if self.points:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    c.fill(path.circle(i, j, 0.1), [color.grey(0.8)])
         
-        # Draw components
+        # draw out all components
         for component in self.components:
-            component.draw(ax)
+            component.draw(c)
+        # Return the canvas
+        # display canvas
+        return c
+    
+    def save(self, filename):
+        # Save the canvas as a PNG file
+        c = self.draw()
+        # filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+        # c.writeGSfile(filename, device="png16m")
+        c.writeSVGfile(filename)
         
-        ax.set_xlim(-1, self.cols)
-        ax.set_ylim(-1, self.rows)
-        plt.show()
+        
+    def propagate(self):
+        pass
