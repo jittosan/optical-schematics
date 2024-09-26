842d49fd30ee53ada2273096e2b073e598bff519
Jitto <64718148+jittosan@users.noreply.github.com>
2024-09-26 16:12:54 +0800

    play around testing

---
+++ playground.py
@@ -0,0 +1,21 @@
+from components import *
+from pyx import canvas, path, color
+
+c = canvas.canvas()
+# Draw a white background
+# c.fill(path.rect(0, 0, 12, 4), [color.rgb.white])
+
+# Draw a simple optical schematic with lenses and mirrors
+mirror = Mirror(4, -6, 30)
+# mirror.draw(c)
+laser = Laser(8, 2, 0)
+laser.draw(c)
+collimator = Collimator(4, 2, 130)
+# collimator.draw(c)
+iris = Iris(0, 2, 0)
+# iris.draw(c)
+
+
+
+# save the canvas to a file
+c.writeSVGfile("playground")
