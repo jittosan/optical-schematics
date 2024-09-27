1e889ffc95862e16a030167c80f8216aabd66a91
Jitto <64718148+jittosan@users.noreply.github.com>
2024-09-27 22:42:45 +0800

    admin

--- draft_setup.py
+++ draft_setup.py
@@ -1,21 +1,34 @@
 from components import *
-from table import OpticalTable
+from setup import OpticalTable
 from pyx import path, color
 
-setup = OpticalTable(30, 30)
+setup = OpticalTable(50, 50)
 setup.points = True
 
 # position all components
-setup.add(Laser(0, 0, 0))
-setup.add(Mirror(20, 0, 45))
-setup.add(Mirror(20, 5, 45+90))
-setup.add(Mirror(10, 5, -45))
-setup.add(PolarisingBeamSplitter(10, 10, 0))
-setup.add(EndBlock(5, 10, 90))
+setup.add(Laser(14, 0, 0))
+setup.add(Mirror(30, 0, 45))
+setup.add(Mirror(30, 5, 45+90))
+setup.add(Mirror(24, 5, -45))
+setup.add(Polariser(24, 8, 0))
+setup.add(PolarisingBeamSplitter(24, 12, 0))
+setup.add(Polariser(24, 16, 0))
+setup.add(EndBlock(30, 12, 90))
+setup.add(Mirror(24, 42, 180))
 
-setup.add(Laser(0, 5, 90))
-setup.add(Mirror(0, 25, 180+45))
-setup.add(Iris(5, 25, 90))
+setup.add(Laser(1, 10, 90))
+setup.add(Mirror(1, 25, 180+45))
+setup.add(Mirror(5, 25, 45))
+setup.add(Mirror(5, 30, 180+45))
+setup.add(Polariser(8, 30, 90))
+setup.add(PolarisingBeamSplitter(11, 30, -90))
+setup.add(Collimator(14, 30, -90))
+setup.add(EndBlock(11, 25, 0))
+
+# include magnetometry components
+setup.add(MuMetalShield(24, 30, 0))
+setup.add(FerriteShield(24, 30, 0))
+setup.add(RbCell(24, 30, 0))
 
 # save
 setup.save("draft_setup")
