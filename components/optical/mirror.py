d9612c43fdcf72a25239bc4382d473e470d1edbb
Jitto <64718148+jittosan@users.noreply.github.com>
2024-09-28 04:36:16 +0800

    refactor components to implement light interactions

--- components/optical/mirror.py
+++ components/optical/mirror.py
@@ -1,13 +1,61 @@
 from pyx import path, style, color, deco
 from .base import OpticalComponent
+from utils import compute_intersected_beam
 
 class Mirror(OpticalComponent):
     def __init__(self, x, y, angle):
         super().__init__(x, y, angle)
-
+        # [height, width]
+        self.dims = [0.5, 2]
+        
+    def interact(self, beam, angle, c):
+        height = self.dims[0]
+        width  = self.dims[1]
+        
+        # outer box that stops propagation
+        outer_box = path.path(
+            path.moveto(*self._rp(-width / 2, -height)),
+            path.lineto(*self._rp(width / 2, -height)),
+            path.lineto(*self._rp(width / 2, 0)),
+            path.lineto(*self._rp(-width / 2, 0)),
+            path.closepath()
+        )
+        
+        
+        
+        
+        # l1, l2 = beam.intersect(outer_box)
+        # if l1 != []: 
+        #     # print(dir(l1[0]))
+        #     a = beam.at(l1[0])
+        #     # print(dir(a))
+        #     print(a)
+        #     # print(dir(a[0]))
+        #     b = path.path(path.moveto(0, 0))
+        #     print(b.atbegin())
+        #     c = path.path(path.moveto(*b.atbegin()))
+        # # for segment in beam.at(l1):
+        # #     c.stroke(segment, [style.linewidth.Thin, color.rgb.red])
+        
+        # inner box that reflects the beam
+        inner_box = path.path(
+            path.moveto(*self._rp(-width / 2, -height / 2)),
+            path.lineto(*self._rp(width / 2, -height / 2)),
+            path.lineto(*self._rp(width / 2, -height)),
+            path.lineto(*self._rp(-width / 2, -height)),
+            path.closepath()
+        )
+        
+        new_beam = compute_intersected_beam(beam, outer_box)
+        if new_beam is None:
+            return beam, None
+        # print(f"INCIDENT: {angle} | NORMAL : {self.angle} | REFLECTED: {2 * self.angle - angle + 180}")
+        return new_beam, [(2 * self.angle - angle + 180) % 360]
+        # return new_beam, None
+        
     def draw(self, c):
-        height = 0.5
-        width  = 2
+        height = self.dims[0]
+        width  = self.dims[1]
         
         # Draw the bounding box
         outer_box = path.path(
--- components/sources/laser.py
+++ components/sources/laser.py
@@ -5,15 +5,18 @@ from .base import OpticalSource
 class Laser(OpticalSource):
     def __init__(self, x, y, angle):
         super().__init__(x, y, angle)
+        # [height, width, source_height, source_width]
+        self.dims = [2, 1, 0.2, 0.5]
         
     def source(self):
-        return self.x, self.y, self.angle
+        x, y = self._rp(0, self.dims[0] / 2)
+        return x, y, self.angle
 
     def draw(self, c):
-        height = 2
-        width  = 1
-        source_height = height / 10
-        source_width = width / 2
+        height = self.dims[0]
+        width  = self.dims[1]
+        source_height = self.dims[2]
+        source_width = self.dims[3]
         
         # Draw the source
         laser_source = path.path(
