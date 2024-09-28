b6c3a7c91253a8cc13986f5da815cbe4746975ef
Jitto <64718148+jittosan@users.noreply.github.com>
2024-09-28 12:34:18 +0800

    implement interaction of end block

--- components/optical/end_block.py
+++ components/optical/end_block.py
@@ -1,13 +1,34 @@
 from pyx import path, style, color, deco
 from .base import OpticalComponent
+from utils import compute_intersected_beam
 
 class EndBlock(OpticalComponent):
     def __init__(self, x, y, angle):
         super().__init__(x, y, angle)
+        # [height, width]
+        self.dims = [0.5, 2]
+        
+    def interact(self, beam, angle):
+        height = self.dims[0]
+        width  = self.dims[1]
+        
+        # outer box that stops propagation
+        outer_box = path.path(
+            path.moveto(*self._rp(-width / 2, -height)),
+            path.lineto(*self._rp(width / 2, -height)),
+            path.lineto(*self._rp(width / 2, 0)),
+            path.lineto(*self._rp(-width / 2, 0)),
+            path.closepath()
+        )
+
+        new_beam = compute_intersected_beam(beam, outer_box)
+        if new_beam is None:
+            return beam, None
+        return new_beam, None
 
     def draw(self, c):
-        height = 0.5
-        width  = 2
+        height = self.dims[0]
+        width  = self.dims[1]
         
         # Draw the bounding box
         outer_box = path.path(
